6b704c25739b26722786dedc86564b1201810eb6
AlanInari <veterandgo@gmail.com>
2024-08-29 23:06:33 -0600

    Cambio de nombre del base: en vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nombre-de-tu-repositorio/', // Reemplaza 'nombre-de-tu-repositorio' con el nombre de tu repositorio
+  base: '/AlanInari/', // Reemplaza 'nombre-de-tu-repositorio' con el nombre de tu repositorio
   build: {
     outDir: 'dist'
   }
