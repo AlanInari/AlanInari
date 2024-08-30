c9faefdf8e27a9d3af42674ac0235a06669db4af
AlanInari <veterandgo@gmail.com>
2024-08-29 23:03:59 -0600

    Actualizacion del archivo vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/nombre-de-tu-repositorio/', // Reemplaza 'nombre-de-tu-repositorio' con el nombre de tu repositorio
+  build: {
+    outDir: 'dist'
+  }
 })
